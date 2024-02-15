c39aa29287560493c2ec25edddfb29bbe70cf0d3
ygursu <yasemingrs@gmail.com>
2024-02-15 22:52:14 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp68/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace klavyekullaniciadisifre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string kul_adi = "aaa";
+            string sifre = "12345";
+
+            Console.WriteLine("Lütfen kullanıcı adınızı girin");
+            string kullaniciadi = Console.ReadLine();
+
+            Console.WriteLine("Lütfen kullanıcı şifrenizi girin");
+            string kullanici_sifre = Console.ReadLine();
+
+            if (kullaniciadi == kul_adi && kullanici_sifre == sifre)
+            {
+                Console.WriteLine("Hoşgeldiniz");
+            }
+            else
+            {
+                Console.WriteLine("Kullanıcı adınız veya şifreniz yanlış");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
